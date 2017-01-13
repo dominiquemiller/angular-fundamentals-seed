8688922cba5a0cbede195cb4b080ab7f4eeb62b2
Dominique Miller <dominique.p.miller@gmail.com>
2017-01-12 21:23:55 -0500

    import passenger dashboard into root module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-
+import { PassengerDashboardModule } from './passengerDashboard/passenger-dashboard.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
+    // angular modules
     BrowserModule,
-    CommonModule
+    CommonModule,
+    // custom modules
+    PassengerDashboardModule
   ],
   bootstrap: [
     AppComponent
