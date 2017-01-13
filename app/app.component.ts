66d9872e38f70807f0b16c84e3fe6081c3f995e1
Dominique Miller <dominique.p.miller@gmail.com>
2017-01-12 21:18:40 -0500

    Inital commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
-  template: `
-    <div class="app">
-      Hello!
-    </div>
-  `
-})
-export class AppComponent {
+  @Component({
+    selector: 'niques-app',
+    styleUrls: ['app.component.scss'],
+    template: `
+      <div class="app">
+        {{ title }}
+      </div>
+      `
+  })
 
-}
+  export class AppComponent {
+    title: string;
+
+    constructor() {
+      this.title = 'My first component';
+    }
+  }
+
